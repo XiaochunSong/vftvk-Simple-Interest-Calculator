c90abc42c9de537c5b9a4e9cc7de79c0b36a2333
XiaochunSong <99657577+XiaochunSong@users.noreply.github.com>
2022-02-18 10:02:33 +1100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute() {
   var years = document.getElementById("years").value;
   var interest = (principal * rate * years) / 100;
   var year = new Date().getFullYear() + parseInt(years);
-  document.getElementById("message").innerHTML = `If you deposit <span class='yellow'>${principal}</span><br> 
+  document.getElementById("message").innerHTML = `If you deposit <span class='yellow'>${principal}</span>, <br> 
   at an interest rate of <span class='yellow'>${rate}</span>. <br>You will receive an amount of <span class='yellow'>${interest}</span> <br>
   in the year <span class='yellow'>${year}</span>`;
 }
