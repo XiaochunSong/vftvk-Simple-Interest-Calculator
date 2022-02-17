0ee1bce5a082c4e12c2d4a026a77b62ccfbe884d
XiaochunSong <99657577+XiaochunSong@users.noreply.github.com>
2022-02-17 23:20:54 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function compute() {
   var principal = document.getElementById("principal").value;
+
+  if (principal < 0) {
+    alert("Please put a positive number.");
+  } else if (principal == "") {
+    alert("please put a positive number.");
+  }
+
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("years").value;
   var interest = (principal * rate * years) / 100;
   var year = new Date().getFullYear() + parseInt(years);
-  document.getElementById("message").innerHTML = `If you deposit ${principal}, 
-  at an interest rate of ${rate}%. You will receive an amount of ${interest}, in the year ${year}`;
+  document.getElementById("message").innerHTML = `If you deposit <span class='yellow'>${principal}</span>, 
+  at an interest rate of <span class='yellow'>${rate}</span>. You will receive an amount of <span class='yellow'>${interest}</span> 
+  in the year <span class='yellow'>${year}</span>`;
 }
 function updateRate() {
   var rateval = document.getElementById("rate").value;
